a2524d250b5aa872bdcf5983c105b2c5a6bd1db8
priyankamarabille1008 <110758614+priyankamarabille1008@users.noreply.github.com>
2023-09-05 23:55:37 +0530

    Delete Other directory

--- Other/Demo.java
+++
@@ -1,9 +0,0 @@
-package Other;
-
-public class Demo {
-	public static void main(String[] args){
-		System.out.println(args[0]);
-		System.out.println(args[1]);
-		}
-}
-//run as---> configuration--->arguments--->pass data(hello world).
--- Other/ExampleJava.java
+++
@@ -1,8 +0,0 @@
-
-package Other;
-import java.io.*;
-public class ExampleJava {
-	public static void main(String[] args) {
-		System.out.println("hello");
-	}
-}
